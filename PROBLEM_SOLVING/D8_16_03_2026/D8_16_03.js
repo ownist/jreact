f6c3c3ecaa745089a2c2cf196c91ab8547d5fc66
ownist <shahedevise@gmail.com>
2026-03-16 06:32:05 +0600

    refactor: simplify character counting logic
    - Replace `|| 0` with explicit `if/else`
    - Update test input string

--- PROBLEM_SOLVING/D8_16_03_2026/D8_16_03.js
+++ PROBLEM_SOLVING/D8_16_03_2026/D8_16_03.js
@@ -4,12 +4,16 @@ function countCharacters(str) {
   for (let i = 0; i <= str.length - 1; i++) {
     const lowerLetter = str[i].toLowerCase();
 
-    letterCountObj[lowerLetter] = (letterCountObj[lowerLetter] || 0) + 1;
+    if (letterCountObj[lowerLetter]) {
+      letterCountObj[lowerLetter] = letterCountObj[lowerLetter] + 1;
+    } else {
+      letterCountObj[lowerLetter] = 1;
+    }
   }
 
   return letterCountObj;
 }
 
-const input = "programming";
+const input = "Hello";
 const output = countCharacters(input);
 console.log(output);
