6115f07529b6c3f967d295bd75b8eead0e73a7fa
ownist <shahedevise@gmail.com>
2026-03-20 03:58:52 +0600

    feat(array-utils): Add new even/odd grouping function
    - Implements new group_even_odd function
    - Groups numbers into even and odd arrays
    - Skips non-numeric values

--- PROBLEM_SOLVING/D12_20_03_2026/D12_20_03_2026.js
+++ PROBLEM_SOLVING/D12_20_03_2026/D12_20_03_2026.js
@@ -20,3 +20,32 @@ function groupEvenOdd(arr) {
 const input = [1, "a", 2, null, 3];
 const output = groupEvenOdd(input);
 console.log(output);
+
+/**
+ * for better and clean version
+ */
+
+const group_even_odd = (arr) => {
+  // delclear object structure
+  const grouped = {
+    even: [],
+    odd: [],
+  };
+
+  for (const num of arr) {
+    // edge case: value ta jodi number type na hoy tahole
+    if (typeof num !== "number") continue;
+
+    if (num % 2 === 0) {
+      grouped.even.push(num);
+    } else {
+      grouped.odd.push(num);
+    }
+  }
+
+  // return grouped object
+  return grouped;
+};
+
+const result = group_even_odd(input);
+console.log(result);
