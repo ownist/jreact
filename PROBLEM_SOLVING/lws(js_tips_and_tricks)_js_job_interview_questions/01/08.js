82ac40bd0dfe5eb26bfd606490e347c1da1d5052
ownist <shahedevise@gmail.com>
2026-04-05 23:32:32 +0600

    perf: measure loop execution time
    - Added performance timing for a loop
    - Logs loop execution duration

---
+++ PROBLEM_SOLVING/lws(js_tips_and_tricks)_js_job_interview_questions/01/08.js
@@ -0,0 +1,13 @@
+// know performance of a task
+
+const startTime = performance.now();
+
+for (let i = 0; i <= 50; i++) {
+  console.log(i);
+}
+
+const endTime = performance.now();
+
+console.log(
+  `loop took ${(endTime - startTime).toFixed(2)} milliseconds to finish`,
+);
