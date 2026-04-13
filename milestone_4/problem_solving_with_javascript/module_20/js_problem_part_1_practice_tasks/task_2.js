674200a05763e2eb25df210d121de108241a8f4b
ownist <shahedevise@gmail.com>
2026-04-13 16:35:07 +0600

    solve: js_practice_problem_part_1 task 2

---
+++ milestone_4/problem_solving_with_javascript/module_20/js_problem_part_1_practice_tasks/task_2.js
@@ -0,0 +1,29 @@
+/**
+ * You are given an array of numbers. Count how many times the a number is repeated in the array.
+ * sample-input: numbers = [5,6,11,12,98, 5]
+ *
+ * find: 5
+ * output: 2
+ *
+ * sample-input:
+ * numbers = [5,6,11,12,98, 5]
+ *
+ * find: 25
+ * output: 0
+ */
+
+function countRepatedNums(arr, find) {
+  let count = 0;
+
+  for (const n of arr) {
+    if (n === find) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+const input = [20, 45, 12, 20, 45, 20];
+const output = countRepatedNums(input, 20);
+console.log(output);
