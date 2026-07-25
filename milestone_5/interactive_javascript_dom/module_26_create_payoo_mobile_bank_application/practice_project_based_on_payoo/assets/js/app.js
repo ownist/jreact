11a44882248e9e8715915e458d2abb166bdc0a0c
ownist <shahedevise@gmail.com>
2026-07-25 22:57:17 +0600

    feat: add password visibility toggle
    - Add eye icon toggle to login password field
    - Add JS listener to show password text on click

--- milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/practice_project_based_on_payoo/assets/js/app.js
+++ milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/practice_project_based_on_payoo/assets/js/app.js
@@ -1,1 +1,7 @@
-console.log("yeah! connected");
+// password show and hide functionality
+const eyeShow = document.querySelector(".eye_show");
+const passWordField = document.getElementById("password_filed");
+
+eyeShow.addEventListener("click", function () {
+  passWordField.type = "text";
+});
